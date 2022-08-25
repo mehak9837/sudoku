2e5f48858980157c53c2603ec405c5c26a870334
mehakgupta <mehakgupta9837@gmail.com>
2022-08-26 02:19:19 +0530

    initial

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
+import numpy as np
+import pandas as pd
 
+dataset = pd.read_csv("/home/specter/Desktop/python/PROJECTS/Sudoku Solver/Sudoku_grid.txt", delimiter='\t',
+                      header=None)
+grid = dataset.iloc[:10, :10].values
+# print(np.matrix(grid))
+call = 0
+
+
+def Check(y, x, n):
+    global grid
+    for i in range(0, 9):
+        if grid[y][i] == n:
+            return False
+    for i in range(0, 9):
+        if grid[i][x] == n:
+            return False
+    X = (x // 3) * 3
+    Y = (y // 3) * 3
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if grid[Y + i][X + j] == n:
+                return False
+    return True
+
+
+def solve():
+    global grid, call
+    for y in range(0, 9):
+        for x in range(0, 9):
+            if grid[y][x] == 0:
+                for n in range(0, 10):
+                    if Check(y, x, n):
+                        grid[y][x] = n
+                        solve()
+                        call += 1
+                        grid[y][x] = 0
+
+                return
+
+    print("no.of Recursion:", call)
+    print(np.matrix(grid))
+    exit(0)
+
+
+solve()
